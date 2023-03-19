eca89a58bdfc5f0c5e04524d2fce58fa775722cc
bvz <bvz@c381.com>
2023-03-19 00:17:50 -0700

    Converted to a dataclass

--- bvzos/copyfiles/copydescriptor.py
+++ bvzos/copyfiles/copydescriptor.py
@@ -1,29 +1,8 @@
-import os.path
-
-"""
-A basic class to hold the info about a single file that is to be copied using deduplication.
-"""
+from dataclasses import dataclass
 
 
-# ======================================================================================================================
+@dataclass
 class Copydescriptor:
-
-    # ------------------------------------------------------------------------------------------------------------------
-    def __init__(self,
-                 source_p,
-                 dest_relative_p,
-                 link_in_place=False):
-        """
-        Initialize the object with the data about the file to be copied.
-        """
-
-        assert type(source_p) is str
-        assert type(dest_relative_p) is str
-        assert type(link_in_place) is bool
-
-        if not os.path.exists(source_p):
-            raise ValueError(f"Source file {source_p} does not exist.")
-
-        self.source_p = source_p
-        self.dest_relative_p = dest_relative_p
-        self.link_in_place = link_in_place
+    source_p: str
+    dest_relative_p: str
+    link_in_place: bool = False
